20bcf0cc078047657e031506c0c1b1df4d556346
Nicole <143754076+NicoleNageli@users.noreply.github.com>
2023-11-16 14:54:24 -0700

    whaaaaa

--- ica/ica12/week12.js
+++ ica/ica12/week12.js
@@ -41,3 +41,20 @@ function testFunction(){
 
 getQuote();
 
+// Define the API URL
+const apiUrl = 'http://thecatapi.com/api/images/get?format=src&type=gif';
+
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
